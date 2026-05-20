f91ddbd478eda1920adebb1db219bda7e46790ee
JAkudike <saxifragejasper@gmail.com>
2026-05-20 16:32:35 +0100

    Update app.py

--- config/app.py
+++ config/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 from attendBlockchain import AttendanceBlockchain
 import json
 from urllib.parse import urlparse, parse_qs
@@ -215,4 +216,5 @@ def sign_attendance():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
